abaac4b588af284aa4be6f1cd6b158a2e16d310f
nanami-krry <2487957970@qq.com>
2023-07-10 00:15:54 +0800

    完成了项目的前期配置，正式开始完成项目的功能

--- src/settings.js
+++ src/settings.js
@@ -1,16 +1,18 @@
 module.exports = {
-
+  // 项目的标题
   title: 'Vue Admin Template',
 
   /**
    * @type {boolean} true | false
    * @description Whether fix the header
    */
+  // 头部是否固定
   fixedHeader: false,
 
   /**
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
+  // 左侧菜单logo是否显示
   sidebarLogo: false
 }
