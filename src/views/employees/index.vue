0ad77bd4ed8f06356a55bf0ee42cd09eec997e35
nanami-krry <2487957970@qq.com>
2023-07-17 17:28:54 +0800

    完成了员工管理界面，动态表单，详情页，利用vue-print-nb完成了打印

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -17,7 +17,13 @@
           <el-table-column label="头像" width="120px" align="center">
             <template slot-scope="{row}">
               <div>
-                <img v-imagerror="require('@/assets/common/bigUserHeader.png')" :src="row.staffPhoto" alt="" style="border-radius: 50%; width: 100px; height: 100px; padding: 10px">
+                <img
+                  v-imagerror="require('@/assets/common/bigUserHeader.png')"
+                  :src="row.staffPhoto"
+                  alt=""
+                  style="border-radius: 50%; width: 100px; height: 100px; padding: 10px"
+                  @click="showQrCode(row.staffPhoto)"
+                >
               </div>
             </template>
           </el-table-column>
@@ -54,6 +60,14 @@
 
     </div>
     <add-employee :show-dialog.sync="showDialog" />
+    <el-dialog
+      title="二维码"
+      :visible.sync="showCodeDialog"
+    >
+      <el-row type="flex" justify="center">
+        <canvas ref="myCanvas" />
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
@@ -62,6 +76,7 @@ import addEmployee from './components/add-employee.vue'
 import { getEmployeeList, delEmployee } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees'
 import { formatDate } from '@/filters'
+import Qrcode from 'qrcode'
 export default {
   components: { addEmployee },
   data() {
@@ -73,7 +88,8 @@ export default {
         size: 10,
         total: 0 // 总数
       },
-      showDialog: false
+      showDialog: false,
+      showCodeDialog: false
     }
   },
   created() {
@@ -153,6 +169,19 @@ export default {
           return item[headers[key]]
         }) // => ["张三", "13811"，"2018","1", "2018", "10002"]
       })
+    },
+    showQrCode(url) {
+      if (url) {
+        this.showCodeDialog = true // 数据更新了 但是我的弹层会立刻出现吗 ？页面的渲染是异步的！！！！
+        // 有一个方法可以在上一次数据更新完毕，页面渲染完毕之后
+        this.$nextTick(() => {
+          // 此时可以确认已经有ref对象了
+          Qrcode.toCanvas(this.$refs.myCanvas, url) // 将地址转化成二维码
+          // 如果转化的二维码后面信息 是一个地址的话 就会跳转到该地址 如果不是地址就会显示内容
+        })
+      } else {
+        this.$message.warning('该用户还未上传头像')
+      }
     }
   }
 }
